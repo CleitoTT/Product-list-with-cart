7f386b9b5b39b8a873e5ea8edd6d92d658bb9568
Leonardo Machado <leo.msouza1706@gmail.com>
2024-10-06 12:19:25 -0300

    Cores talwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors:{
+      Red: '#c73a0f',
+      Green: '#1ea475',
+      Rose50: '#fcf9f7',
+      Rose100: '#f4edeb',
+      Rose300: '#c9aea6',
+      Rose400: '#ad8985',
+      Rose500: '#87635a',
+      Rose900: '#260f08'
+
+    },
     extend: {},
   },
   plugins: [],
